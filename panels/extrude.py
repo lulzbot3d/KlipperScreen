78321266f20824da827b254b97fc473abe786f7a
Carl Smith <csmith@lulzbot.com>
2024-05-03 11:42:49 -0500

    Change Temperature button in Extrude panel to read "Set Temperature."

--- panels/extrude.py
+++ panels/extrude.py
@@ -39,7 +39,7 @@ class Panel(ScreenPanel):
             'load': self._gtk.Button("arrow-down", _("Load"), "color3"),
             'unload': self._gtk.Button("arrow-up", _("Unload"), "color2"),
             'retract': self._gtk.Button("retract", _("Retract"), "color1"),
-            'temperature': self._gtk.Button("heat-up", _("Temperature"), "color4"),
+            'temperature': self._gtk.Button("heat-up", _("Set Temperature"), "color4"),
             'spoolman': self._gtk.Button("spoolman", "Spoolman", "color3"),
         }
         self.buttons['extrude'].connect("clicked", self.extrude, "+")
